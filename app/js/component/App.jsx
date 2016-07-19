a77869532d0b38efebec72fb5d0fff500b40bbfa
U-Zyn Chua <chua@uzyn.com>
2016-07-19 12:25:41 +0800

    Web3 migrated

--- app/js/component/App.jsx
+++ app/js/component/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Web3 from './Web3';
 
-const App = () => (
-  <div>
-		Hello world
+export default function App() {
+  return (
+  <div className="app">
+    <h1>Sample Ethereum Dapp with Webpack</h1>
+    <p>Example The Coin Dapp with smart contract code from <a href="https://www.ethereum.org/token" target="_blank">https://www.ethereum.org/token</a></p>
+
+    <hr />
+    <Web3 />
+
+    <hr />
+
+    <hr />
+    <h2>tokenRecipient</h2>
+    <p>
+      If a <code>.sol</code> file contains more than one contracts,
+      individual instantiated contracts are also available.
+      See <code>console.log(tokenRecipient)</code>.
+    </p>
+
+    <hr />
+    <h2>More info</h2>
+    <p>View <code>console.log()</code> for more information of each of the above components.</p>
+
+    <hr />
+    <p>You can find more information on this sample dapp at its <a href="https://github.com/uzyn/react-ethereum-dapp-template" target="_blank">GitHub repository</a> and <a href="https://twitter.com/uzyn" target="_blank">@uzyn</a>.</p>
   </div>
-);
-
-export default App;
+  );
+}
